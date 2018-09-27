649ed26e5f2ee3312dfce31241a9d39944ffe53c
Alex Hideki Makiyama <hidekimaki@hotmail.com>
2018-09-27 11:22:42 -0300

    aaaaaaaaa

--- vraptor-blank/src/main/java/Controllers/Conversor.java
+++ vraptor-blank/src/main/java/Controllers/Conversor.java
@@ -5,14 +5,10 @@ import br.com.caelum.vraptor.Convert;
 import br.com.caelum.vraptor.converter.Converter;
 import javax.enterprise.context.ApplicationScoped;
 import org.bson.types.ObjectId;
-
-public class Conversor {
- 
 @Convert(ObjectId.class)
 @ApplicationScoped
-public class ObjectIdVRaptorConverter implements Converter<ObjectId> {
-
- @Override
+public class Conversor implements Converter<ObjectId> {
+  @Override
  public ObjectId convert(String string, Class<? extends ObjectId> type) {
    try{
      return new ObjectId(string);
@@ -21,4 +17,4 @@ public class ObjectIdVRaptorConverter implements Converter<ObjectId> {
    }
  }
 }
-}
+
